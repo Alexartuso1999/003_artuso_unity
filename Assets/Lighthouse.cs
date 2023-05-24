6ede555d4309942c85bac2dfdd49c34aca79f860
Alex Artuso <Alexartuso1999@gmail.com>
2023-05-24 17:47:59 +0200

    script light

--- Assets/Lighthouse.cs
+++ Assets/Lighthouse.cs
@@ -7,10 +7,11 @@ public class Lighthouse : MonoBehaviour
     public GameObject targetObject;
 
     public float repeatTime = 1f;
+    public float delay = 0f;
 
     private void Start()
     {
-        InvokeRepeating("ChangeState", 1f, repeatTime);
+        InvokeRepeating("ChangeState", 1f, repeatTime + delay);
 
     }
 
--- Assets/Modulo LD/Script/NewMove.cs
+++ Assets/Modulo LD/Script/NewMove.cs
@@ -10,6 +10,11 @@ public class NewMove : MonoBehaviour
     private float moveHorizontal;
     public float maxVelocity = 5;
 
+    Ray ray;
+    public float rayDistance = 5f;
+    public LayerMask layer;
+    public LayerMask layerWall;
+
     private void Start()
     {
         rgb = GetComponent<Rigidbody>();
@@ -19,6 +24,12 @@ public class NewMove : MonoBehaviour
     {
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         rgb.velocity = Vector3.ClampMagnitude(rgb.velocity, maxVelocity);
+
+       //ray = new Ray(transform.position, transform.right * (-1f));
+        //Debug.DrawRay(transform.position, transform.right * (-1f), Color.red);
+
+        CheckLight();
+
     }
 
     private void FixedUpdate()
@@ -26,6 +37,20 @@ public class NewMove : MonoBehaviour
         if(moveHorizontal > 0.1f || moveHorizontal < -0.1f)
         {
             rgb.AddForce(transform.forward * moveHorizontal * moveSpeed, ForceMode.Acceleration);
+        }
+    }
+
+    void CheckLight()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.right * (-1f), out hit, rayDistance, layer))
+        {
+            Debug.DrawRay(transform.position, transform.right * (-1f), Color.red, rayDistance);
+            GameObject.Destroy(gameObject);
+        }
+        else if (Physics.Raycast(transform.position, transform.right * (-1f), out hit, rayDistance, layerWall))
+        {
+
         }
     }
 }

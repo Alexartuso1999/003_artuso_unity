91d438eb1c3d68f660a0ca3cae04e3d2a21c7526
Alexartuso1999 <alexartuso1999@gmail.com>
2023-05-29 22:17:18 +0200

    NICE DIOCAN

---
+++ Assets/Modulo LD/Script/heavyBox.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heavyBox : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+    private Vector3 move;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            move = transform.TransformDirection(-Vector3.right); 
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            move *= -1;
+        }
+    }
+    private void Update()
+    {
+        transform.Translate(move * speed * Time.deltaTime);
+    }
+}
